7c893ae901e5b11f534237c901aa217990762e4e
venkatesh c <venkat@print2block.com>
2024-05-03 22:15:16 +0530

    edit movie route added

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import Login from "./Pages/Login/index.svelte"
 import AddNewMovie from "./Pages/Movies/AddMovies/index.svelte"
 import MoviesList from "./Pages/Movies/MoviesList/index.svelte"
 import MovieDetails from "./Pages/Movies/singleMovieCard/index.svelte"
+import EditMovie from "./Pages/Movies/EditMovie/index.svelte"
 
 export default {
   "/": Login,
   "/Movies/AddNewMovie": AddNewMovie,
   "/Movies/MoviesList": MoviesList,
   "/Movies/MovieDetails": MovieDetails,
+  "/Movies/EditMovie": EditMovie,
   
   // The catch-all route must always be last
   "*": ErrorCode404,
